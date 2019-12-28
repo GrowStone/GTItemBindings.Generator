2b6e29d0a6a4e20825eec351843054b588268a08
Mempler <me@mempler.de>
2019-12-28 17:58:22 +0100

    Adding Files.

---
+++ GTItemBindings.Generator/Program.cs
@@ -0,0 +1,10 @@
+﻿﻿ namespace GTItemBindings.Generator
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            XamlProcessor.Process();
+        }
+    }
+}
